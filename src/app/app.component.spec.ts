0d8f4139065136c992c02c7471f52e75850d40e1
Allie <alinaabdurshina@gmail.com>
2021-09-02 14:11:35 +0300

    rename rim-test -> rimworld-popchart-app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'rim-test'`, () => {
+  it(`should have as title 'rimworld-popchart-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rim-test');
+    expect(app.title).toEqual('rimworld-popchart-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('rim-test app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('rimworld-popchart-app app is running!');
   });
 });
