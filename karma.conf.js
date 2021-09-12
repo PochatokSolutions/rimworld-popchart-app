de891f8de8afb71546edd740e66e9e46dd212196
Allie <alinaabdurshina@gmail.com>
2021-09-12 13:33:41 +0300

    Rename rim-test -> rimworld-popchart-app

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/rim-test'),
+      dir: require('path').join(__dirname, './coverage/rimworld-popchart-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
